39b11651f6d136ae1244ca34c95886157c7d90b1
OrionBarly <barlyvalentina@gmail.com>
2022-12-29 01:00:40 -0300

    agregadas tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,15 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Terminar front end',
+    day: 'Diciembre 30 a las 17:00',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Terminar back end',
+    day: 'Enero 10 a las 17:00',
+    reminder: true,
+  },
+];
